d98add211507a8e06a4b15d9a39dc25c50fbe760
Lilithme <164787799+Lilithme@users.noreply.github.com>
2024-06-02 20:48:25 +0200

    bilder

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let Button = document.getElementById("keks")
-console.log(Button)
-Button.addEventListener("click",togglemode)
+// let Button = document.getElementById("keks")
+// console.log(Button)
+// Button.addEventListener("click",togglemode)
 
-function togglemode(){
-    if(document.body.classList.contains("dark")){
-        document.body.classList.remove("dark")
-        document.body.classList.add("light")
-    }else{
-        document.body.classList.remove("light")
-        document.body.classList.add("dark")
-    }
-}
+// function togglemode(){
+//     if(document.body.classList.contains("dark")){
+//         document.body.classList.remove("dark")
+//         document.body.classList.add("light")
+//     }else{
+//         document.body.classList.remove("light")
+//         document.body.classList.add("dark")
+//     }
+// }
 
 let nextDom = document.getElementById("next")
 let prevDom = document.getElementById("prev")
